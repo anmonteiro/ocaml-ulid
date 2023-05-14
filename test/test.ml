b9432e71d330a60855e23e88594e2eec54e0f4e6
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-05-14 15:45:29 -0700

    get tests running

--- test/test.ml
+++ test/test.ml
@@ -1,10 +1,16 @@
+let prng i =
+  let z = Mirage_crypto_pk.Z_extra.gen (Z.of_int i) in
+  Z.to_int z
+
+let seed_time () = 1000. *. Unix.gettimeofday () |> Int.of_float
+
 (* The tests *)
-let increment_base32_1 () = 
+let increment_base32_1 () =
   Alcotest.(check string) "increments correctly" "A109D" (Ulid.increment_base_32 "A109C")
 
 let increment_base32_2 () =
   Alcotest.(check string) "carries correctly" "A1Z00" (Ulid.increment_base_32 "A1YZZ")
-  
+
 let increment_base32_3 () =
   Alcotest.(check string) "double increments correctly" "A1Z01" (Ulid.increment_base_32 (Ulid.increment_base_32 "A1YZZ"))
 
@@ -30,17 +36,20 @@ let test_set_2 = [
 ]
 
 let encode_random_1 () =
-  Alcotest.(check int) "returns current length" 12 (String.length (Ulid.encode_random 12 (Ulid.get_nocrypto_rng ())))
+  Alcotest.(check int) "returns current length" 12 (String.length (Ulid.encode_random 12 prng))
 
 let test_set_3 = [
   "encode random returns correct length", `Quick, encode_random_1;
 ]
 
 let ulid_1 () =
-  Alcotest.(check int) "returns current length" 26 (String.length (Ulid.ulid ()))
+  Alcotest.(check int)
+    "returns current length"
+    26
+    (String.length (Ulid.ulid ~seed_time:(seed_time()) ~prng ()))
 
 let ulid_2 () =
-  let ulid = (Ulid.ulid ~seed_time:1469918176385 ()) in
+  let ulid = (Ulid.ulid ~seed_time:1469918176385 ~prng ()) in
   let time = (String.sub ulid 0 10) in
   Alcotest.(check string) "returns expected time encoded" "01ARYZ6S41" time
 
@@ -62,22 +71,22 @@ let test_set_4 = [
 ]
 
 let stubbed_rng n = (n * 0) + 30;;
-let monotonic_ulid = Ulid.monotonic_factory ~prng:stubbed_rng ();;
+let monotonic_ulid = Ulid.monotonic_factory ~prng:stubbed_rng ;;
 
 let monotonic_1 () =
-  Alcotest.(check string) "first call" "01ARYZ6S41YYYYYYYYYYYYYYYY" (monotonic_ulid ~seed_time:1469918176385 ())
+  Alcotest.(check string) "first call" "01ARYZ6S41YYYYYYYYYYYYYYYY" (monotonic_ulid ~seed_time:1469918176385)
 
 let monotonic_2 () =
-  Alcotest.(check string) "second call same time" "01ARYZ6S41YYYYYYYYYYYYYYYZ" (monotonic_ulid ~seed_time:1469918176385 ())
+  Alcotest.(check string) "second call same time" "01ARYZ6S41YYYYYYYYYYYYYYYZ" (monotonic_ulid ~seed_time:1469918176385)
 
 let monotonic_3 () =
-  Alcotest.(check string) "third call smaller time" "01ARYZ6S41YYYYYYYYYYYYYYZ0" (monotonic_ulid ~seed_time:100000000 ())
+  Alcotest.(check string) "third call smaller time" "01ARYZ6S41YYYYYYYYYYYYYYZ0" (monotonic_ulid ~seed_time:100000000)
 
 let monotonic_4 () =
-  Alcotest.(check string) "fourth call even smaller time" "01ARYZ6S41YYYYYYYYYYYYYYZ1" (monotonic_ulid ~seed_time:10000 ())
+  Alcotest.(check string) "fourth call even smaller time" "01ARYZ6S41YYYYYYYYYYYYYYZ1" (monotonic_ulid ~seed_time:10000)
 
 let monotonic_5 () =
-  Alcotest.(check string) "fifth call 1 greater time" "01ARYZ6S42YYYYYYYYYYYYYYYY" (monotonic_ulid ~seed_time:1469918176386 ())
+  Alcotest.(check string) "fifth call 1 greater time" "01ARYZ6S42YYYYYYYYYYYYYYYY" (monotonic_ulid ~seed_time:1469918176386)
 
 let test_set_5 = [
   "monotonic first call", `Quick, monotonic_1;
@@ -89,6 +98,7 @@ let test_set_5 = [
 
 (* Run it *)
 let () =
+  Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna);
   Alcotest.run "ULID tests" [
     "base 32 increment", test_set_1;
     "encode time", test_set_2;
